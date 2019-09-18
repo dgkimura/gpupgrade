c60bbb83f0af79427bd4bb5665031e1a699e7ac5
Ed Espino <eespino@pivotal.io>
2019-09-17 21:52:09 -0700

    Display Concourse cache directory after it is populated..

--- ci/scripts/gen-go-dep-cache.bash
+++ ci/scripts/gen-go-dep-cache.bash
@@ -3,11 +3,11 @@
 set -ex
 
 # If it exists, show contents of cache directory
-if [ -d go/pkg/dep ]; then
-    echo "Concourse cache directory: go/pkg/dep"
-    ls -al go/pkg/dep
+if [ -d go/pkg/dep/sources ]; then
+    echo "Concourse cache directory: go/pkg/dep/sources"
+    ls -al go/pkg/dep/sources
 else
-    echo "go/pkg/dep does not exist"
+    echo "go/pkg/dep/sources does not exist"
 fi
 
 # make depend fails if run as gpadmin with a dep ensure git-remote-https signal 11 failure
@@ -15,5 +15,12 @@ export GOPATH="$PWD/go"
 export PATH="$PWD/go/bin:$PATH"
 
 make -C go/src/github.com/greenplum-db/gpupgrade dep
+
+if [ -d go/pkg/dep/sources ]; then
+    echo "Concourse cache directory: go/pkg/dep/sources"
+    ls -al go/pkg/dep/sources
+else
+    echo "go/pkg/dep/sources does not exist"
+fi
 
 tar zcf go/pkg/go-dep-cache.tgz -C go/pkg dep
