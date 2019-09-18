53af03d71adb844271876e2c9d2f46bfadfcee45
Ed Espino <eespino@pivotal.io>
2019-09-17 21:46:20 -0700

    Display Concourse cache directory.

--- ci/scripts/gen-go-dep-cache.bash
+++ ci/scripts/gen-go-dep-cache.bash
@@ -2,6 +2,14 @@
 
 set -ex
 
+# If it exists, show contents of cache directory
+if [ -d go/pkg/dep ]; then
+    echo "Concourse cache directory: go/pkg/dep"
+    ls -al go/pkg/dep
+else
+    echo "go/pkg/dep does not exist"
+fi
+
 # make depend fails if run as gpadmin with a dep ensure git-remote-https signal 11 failure
 export GOPATH="$PWD/go"
 export PATH="$PWD/go/bin:$PATH"
